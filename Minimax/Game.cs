d1f83e95ac18de34d785717775ebea3ebc6917b4
Your Name <you@example.com>
2019-02-14 15:11:43 +0000

    update

---
+++ Minimax/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimax
+{
+    class Game
+    {
+        GameBoard board = new GameBoard('-');
+
+        public Game(Player _xPlayer, Player _oPlayer)
+        {
+            PlayGame(_xPlayer, _oPlayer);
+        }
+
+        public void PlayGame(Player currentPlayer, Player otherPlayer)
+        {
+            board.DisplayBoard();
+            Tuple<int, int> selectedMove = currentPlayer.GetMove(board);
+            //MINMAX
+            board[selectedMove.Item1, selectedMove.Item2] = currentPlayer.counter;
+            if (IsOver(board, currentPlayer))
+            {
+                if (currentPlayer.Win(board, currentPlayer.counter))
+                {
+                    board.DisplayBoard();
+                    if (currentPlayer.GetType() == typeof(AIPlayer))
+                    {
+                        Console.WriteLine("The computer has beaten you. Better luck next time...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Congratulations! {0} has won!", currentPlayer.name);
+                    }
+                    Console.ReadLine();
+                    Program.Main();
+                }
+                Console.WriteLine("The game is a draw.");
+                Console.ReadLine();
+                Program.Main();
+            }
+            PlayGame(otherPlayer, currentPlayer);
+        }
+
+        public bool IsOver(GameBoard board, Player currentPlayer)
+        {
+            if (currentPlayer.Win(board, currentPlayer.counter) || board.IsFull())
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Minimax/HumanPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimax
+{
+    class HumanPlayer : Player
+    {
+        public HumanPlayer(string _name, char _counter) : base(_counter)
+        {
+            name = _name;
+        }
+
+        public override Tuple<int, int> GetMove(GameBoard board)
+        {
+            int x;
+            int y;
+            Console.WriteLine("It's {0}'s turn.", name);
+            Console.WriteLine();
+            do
+            {
+                Console.Write("Enter x coordinate: ");
+                while (!(int.TryParse(Console.ReadKey().KeyChar.ToString(), out x) && (x >= 1 && x <= 7)))
+                    Console.Write("\nInvalid input. Try again: ");
+                Console.Write("\nEnter y coordinate: ");
+                while (!(int.TryParse(Console.ReadKey().KeyChar.ToString(), out y) && (y >= 1 && y <= 7)))
+                    Console.Write("\nInvalid input. Try again: ");
+            } while (!CheckValidMove(board, x, y));
+            return new Tuple<int, int>(x, y);
+        }
+
+        public bool CheckValidMove(GameBoard board, int x, int y)
+        {
+            if (board[x, y] == '-')
+                return true;
+            Console.WriteLine("\nThere is already a counter at ({0}, {1}). Try again.", x, y);
+            // Debug.Assert();
+            return false;
+        }
+    }
+}
